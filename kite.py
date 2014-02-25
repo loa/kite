8306e28ad277151a1b47dbf0881695e646cd9c34
Carl Loa Odin <carl.loa.odin@klarna.com>
2014-02-25 13:05:37 +0100

    Added stomp support

--- kite.py
+++ kite.py
@@ -3,13 +3,23 @@
 
 import ConfigParser, os, sys, subprocess, glob, json
 import requests, hmac, base64, hashlib
+from stompest.config import StompConfig
+from stompest.sync import Stomp
 
 class Kite:
-  host = None
-  port = None
-  apikey = None
-  secretkey = None
-  apiurl = None
+  use_hooks = False
+  use_stomp = False
+
+  cloudstack_host      = None
+  cloudstack_port      = None
+  cloudstack_apikey    = None
+  cloudstack_secretkey = None
+  cloudstack_apiurl    = None
+
+  stomp_uri      = None
+  stomp_login    = None
+  stomp_passcode = None
+  stomp_queue    = None
 
   def __init__(self):
     self.read_config()
@@ -20,17 +30,26 @@ class Kite:
       config = ConfigParser.RawConfigParser()
       config.read("%s/%s" % (os.path.dirname(sys.argv[0]), 'kite.cfg'))
 
-      self.host = config.get('Cloudstack', 'host')
-      self.port = config.get('Cloudstack', 'port')
-      self.apikey = config.get('Cloudstack', 'apikey')
-      self.secretkey = config.get('Cloudstack', 'secretkey')
+      self.use_hooks = config.get('Kite', 'use_hooks')
+      self.use_stomp = config.get('Kite', 'use_stomp')
+
+      self.cloudstack_host      = config.get('Cloudstack', 'host')
+      self.cloudstack_port      = config.get('Cloudstack', 'port')
+      self.cloudstack_apikey    = config.get('Cloudstack', 'apikey')
+      self.cloudstack_secretkey = config.get('Cloudstack', 'secretkey')
+
+      self.stomp_uri      = config.get('Stomp', 'uri')
+      self.stomp_login    = config.get('Stomp', 'login')
+      self.stomp_passcode = config.get('Stomp', 'passcode')
+      self.stomp_queue    = config.get('Stomp', 'queue')
+
     except Exception as e:
       print "Error in kite.cfg"
       print e
       sys.exit(1)
 
     # Create url from config settings
-    self.apiurl = "http://%s:%s/client/api" % (self.host, self.port)
+    self.apiurl = "http://%s:%s/client/api" % (self.cloudstack_host, self.cloudstack_port)
 
   def get_jobs(self):
     # Read the list of processed jobs as a json file from kite directory
@@ -56,6 +75,11 @@ class Kite:
     # New list of all jobs that still appears from api and new processed ones
     processed_jobs = []
 
+    if self.use_stomp:
+      stomp_config = StompConfig(self.stomp_uri, login=self.stomp_login, passcode=self.stomp_passcode)
+      stomp_client = Stomp(stomp_config)
+      stomp_client.connect()
+
     for job in self.request('listAsyncJobs'):
       # Add job to new processed list
       processed_jobs.append(job['jobid'])
@@ -64,22 +88,31 @@ class Kite:
       if job['jobid'] in prev_processed_jobs:
         continue
 
-      if job['jobstatus'] == 1:
-        # Flatten tree structure
-        params = self.parse_dict(job['jobresult'])
+      if self.use_hooks:
+        if job['jobstatus'] == 1:
+          # Flatten tree structure
+          params = self.parse_dict(job)
 
-        if job['cmd'] == 'com.cloud.api.commands.DestroyVMCmd':
-          self.trigger_hooks('vmdestroy', params)
+          if job['cmd'] == 'com.cloud.api.commands.DestroyVMCmd':
+            self.trigger_hooks('vmdestroy', params)
 
-        elif job['cmd'] == 'com.cloud.api.commands.DeployVMCmd':
-          self.trigger_hooks('vmdeploy', params)
+          elif job['cmd'] == 'com.cloud.api.commands.DeployVMCmd':
+            self.trigger_hooks('vmdeploy', params)
+
+      if self.use_stomp:
+        print "Stomp: %s" % params['jobid']
+        stomp_client.send(self.stomp_queue, job)
+
+    if self.use_stomp:
+      stomp_client.disconnect()
+
 
     # Save list of processed jobs
     self.save_jobs(processed_jobs)
 
   def request(self, command, params = {}):
     # Set basic params
-    params['apikey'] = self.apikey
+    params['apikey'] = self.cloudstack_apikey
     params['command'] = command
     params['response'] = 'json'
 
@@ -89,7 +122,7 @@ class Kite:
 
     # Create signature with the queries and the secret key
     signature = base64.b64encode(hmac.new(
-      self.secretkey,
+      self.cloudstack_secretkey,
       msg=prereq.path_url.lower()[2:],
       digestmod=hashlib.sha1
     ).digest())
@@ -128,7 +161,7 @@ class Kite:
     for file in glob.glob("%s%s-*" % (hooks_dir, hook)):
       # Make sure file is executable
       if os.access(file, os.X_OK):
-        print "Trigger: %s" % file
+        print "Trigger: %s for %s" % (file, params['jobid'])
 
         # Run hook with job vars as env variables
         subprocess.call([file], env=params, shell=True, executable="/bin/bash")
